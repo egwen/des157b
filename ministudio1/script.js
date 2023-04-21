dc9fd34e60ef83c96c1c0e116e1d64c6ecd24ed8
egwen <egwen@ucdavis.edu>
2023-04-21 15:54:19 -0700

    video pod styling

--- ministudio1/script.js
+++ ministudio1/script.js
@@ -6,11 +6,7 @@
     backgroundVideo.playbackRate = 0.8;
     let backgroundVideoSrcs = document.querySelectorAll('#background-video video source');
     let currentVideo = 'water';
-    // let videosLoaded = false;
-    // let longestVideo = document.getElementById('earth-video');
-    // longestVideo.addEventListener('playing', () => {
-    //     videosLoaded = true;
-    // });
+    
     let videoPage = document.getElementById('videos');
     // videoPage.style.display = 'none';
     let poemPage =  document.getElementById('poem');
@@ -18,6 +14,7 @@
     const line2 = document.querySelector('#line2');
     const line3 = document.querySelector('#line3');
     const poem = {
+        videoType: ['water', 'air', 'earth'],
         width: [305, 457, 650],
         line: [line1, line2, line3],
         content: ['How rare', 'and beautiful it is', 'to even exist...']
@@ -25,12 +22,14 @@
     let line = 0;
 
     let intervalNum = 0;
-    let interval = setInterval(typeLine, 200);
+    switchBackgroundVideo(poem.videoType[line], 0.2);
+    let interval = setInterval(typeLine, 150);
 
     function typeLine() {
         intervalNum++;
         poem.line[line].textContent = poem.content[line].slice(0, intervalNum);
-        if (intervalNum > poem.content[line].length) {
+        if (intervalNum >= poem.content[line].length) {
+
             intervalNum = 0;
             if (line < poem.line.length - 1) {
                 poem.line[line].style.animation = '';
@@ -38,30 +37,31 @@
             }
             nextLine();
         }
+        
     }
 
     function nextLine(){
+        poem.line[line].className = "hidden";
+
         if (line < poem.line.length - 1) {
-            poem.line[line].className = "hidden";
             line++;
             poem.line[line].className = 'showing';
         } else {
             clearInterval(interval);
-            // console.log(videosLoaded)
+
             setTimeout(() => {
+                backgroundVideo.style.opacity = 0.5;
+
                 poem.className = 'hidden';
 
                 videoPage.style.display = 'flex';
                 videoPage.className = 'showing';
-                document.getElementById('background-video').className = 'showing';            
-            }, 2000);
-            
+                document.getElementById('background-video').className = 'showing';   
+         
+            }, 1000);
         }
+
     }
-    
-
-    
-
     
     console.log(backgroundVideoSrcs)
     videos.forEach((video) => {
@@ -70,15 +70,19 @@
             console.log(video.id, currentVideo);
             if (currentVideo != video.id) {
                 currentVideo = video.id;
-                backgroundVideo.style.opacity = 0.15;
-                setTimeout(() => {
-                    backgroundVideoSrcs.forEach((source) => {
-                        source.src = `media/${currentVideo}.${source.type.slice(6)}`;
-                    });
-                    backgroundVideo.load();
-                    backgroundVideo.style.opacity = 0.5;
-                }, 500);
+                switchBackgroundVideo(currentVideo, 0.5);
             }
         });
     });
+
+    function switchBackgroundVideo(currentVideo, opacity) {
+        backgroundVideo.style.opacity = 0;
+        setTimeout(() => {
+            backgroundVideoSrcs.forEach((source) => {
+                source.src = `media/${currentVideo}.${source.type.slice(6)}`;
+            });
+            backgroundVideo.load();
+            backgroundVideo.style.opacity = opacity;
+        }, 250);
+    }
 })();
